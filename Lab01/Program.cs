7dd03cc88d3569b4215358b83be37d7330915d37
qazannabe <qazannabe@gmail.com>
2026-09-14 21:33:22 +0300

    Lab01 : variables and interpolation

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string myName = "Александр Дегтярев";
+string groupName = "РУПО-265";
+int courseNumber = 2;
+double averageGrade = 4.6;
+bool isBudget = true;
+
+Console.WriteLine("Знакомство");
+Console.WriteLine($"Студент: {myName}");
+Console.WriteLine($"Группа: {groupName}");
+Console.WriteLine($"Курс: {courseNumber}");
+Console.WriteLine($"Средний балл: {averageGrade}");
+Console.WriteLine($"Бюджетное место: {isBudget}");
