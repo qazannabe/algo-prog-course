08e728e26264c4236bfe9cf03ec965cf9d890ef6
qazannabe <qazannabe@gmail.com>
2026-09-20 15:37:47 +0300

    Lab01 task 2: temperature convert

---
+++ TempConverter/Program.cs
@@ -0,0 +1,11 @@
+﻿double celsius = 19.3;
+
+const double FahrenheitScale = 9.0 / 5.0;
+const double FahrenheitOffset = 32.0; 
+const double KelvinOffset = 273.15;
+
+double fahrenheit = celsius * FahrenheitScale + FahrenheitOffset;
+
+double kelvin = celsius + KelvinOffset;
+
+Console.WriteLine($"{celsius}°C = {fahrenheit}°F = {kelvin}K");
